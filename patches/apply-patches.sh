cfbd0e865bec6760b1e8e3773d48876044f5228f
imranpopz <imranpopz32@gmail.com>
2018-08-15 15:39:04 +0530

    H30U10 : Add EarlySuspend Patch

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -2,6 +2,7 @@
 cd ../../../..
 cd system/core
 patch -p1 < ../../device/huawei/h30u10/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
+patch -p1 < ../../device/huawei/h30u10/patches/earlysuspend.patch
 cd ../..
 cd bionic
 patch -p1 < ../device/huawei/h30u10/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
