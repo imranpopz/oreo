641bab298932241fd854e982be96bb2170a96ae0
Manjot Sidhu <manjot.gni@gmail.com>
2018-09-16 19:37:03 +0530

    Overide CameraParameters.h

--- lineage_h30u10.mk
+++ lineage_h30u10.mk
@@ -135,9 +135,9 @@ PRODUCT_PACKAGES += \
 	Snap
 
 PRODUCT_PACKAGES += \
-    camera.device@1.0-impl-legacy \
+    camera.device@1.0-impl \
     camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
 # Wifi
