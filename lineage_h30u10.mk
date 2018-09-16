e057335abe28a533adf392119257664edfdd4873
Manjot Sidhu <manjot.gni@gmail.com>
2018-09-16 19:40:10 +0530

    Camera: Add libcam.client wrapper

--- lineage_h30u10.mk
+++ lineage_h30u10.mk
@@ -135,6 +135,7 @@ PRODUCT_PACKAGES += \
 	Snap
 
 PRODUCT_PACKAGES += \
+	libcam.client \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
